28d6297df03e03ffc79bef820857b63f10a52ab7
keedpek <zaharv20004@gmail.com>
2025-06-11 01:58:38 +0300

    feat: done routing

---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+const NotFoundPage: FC = () => {
+  return <div>404 ALARM BEDA BEDA</div>
+}
+
+export default NotFoundPage
---
+++ src/pages/RegistrationPage/RegistrationPage.tsx
@@ -0,0 +1,7 @@
+import { FC } from 'react'
+
+const RegistrationPage: FC = () => {
+  return <div>regPage</div>
+}
+
+export default RegistrationPage
