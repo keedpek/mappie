fc66ecb7152de79d258042528763bfbb12819f1e
keedpek <zaharv20004@gmail.com>
2025-06-15 18:29:25 +0300

    feat: make login with google

--- src/components/layout/Authentication/GoogleBtn/GoogleBtn.tsx
+++ src/components/layout/Authentication/GoogleBtn/GoogleBtn.tsx
@@ -1,13 +1,47 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import style from './GoogleBtn.module.css'
 import google from '@/assets/google-icon-logo.svg'
+import { signInWithPopup } from 'firebase/auth'
+import { auth, googleProvider } from '@/firebase'
+import { useDispatch } from 'react-redux'
+import { setEmail, setIsAuth } from '@/store/slices/userSlice'
+import { formatAuthError } from '@/utils/authErrorsParser'
+import { useNavigate } from 'react-router-dom'
+import { MAIN_ROUTE } from '@/constants/routes'
 
 const GoogleBtn: FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [error, setError] = useState('')
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const handleLogin = async () => {
+    try {
+      setIsLoading(true)
+      const response = await signInWithPopup(auth, googleProvider)
+      console.log(response)
+      dispatch(setIsAuth(true))
+      dispatch(setEmail(response.user.email))
+      navigate(MAIN_ROUTE)
+    } catch (error) {
+      setError(formatAuthError(error.message))
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
   return (
-    <button className={style.googleBtn}>
-      <img src={google} />
-      <span>Войти с помощью Google</span>
-    </button>
+    <>
+      <button
+        className={style.googleBtn}
+        onClick={handleLogin}
+        disabled={isLoading}
+      >
+        <img src={google} />
+        <span>Войти с помощью Google</span>
+      </button>
+      {error && <p className={style.error}>{error}</p>}
+    </>
   )
 }
 
--- src/utils/authErrorsParser.ts
+++ src/utils/authErrorsParser.ts
@@ -1,5 +1,8 @@
 export const formatAuthError = (error: string) => {
   if (error.includes('invalid-credential')) {
     return 'Некорректные email или пароль'
+  } else if (error.includes('email-already-in-use')) {
+    return 'Пользователь с таким email уже зарегистрирован'
   }
+  return error
 }
