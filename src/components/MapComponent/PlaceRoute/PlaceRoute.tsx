89d25ba35112277b4da6faf7eb8412ba8174bfcc
keedpek <zaharv20004@gmail.com>
2025-06-30 00:26:38 +0300

    fix: some routing bugs

--- src/components/MapComponent/PlaceRoute/PlaceRoute.tsx
+++ src/components/MapComponent/PlaceRoute/PlaceRoute.tsx
@@ -5,7 +5,9 @@ import { useMap } from 'react-leaflet'
 import { getOSRMRoute } from '@/api/search'
 import { ROUTELINE_STYLE } from '@/constants/LeafletStyles/routeLineStyle'
 import { setRoutePlace } from '@/store/slices/mapSlice'
+import Loader from '@/UI/Loader/Loader'
 import { useAppDispatch } from '@/utils/hooks/reduxHooks'
+import { useToast } from '@/utils/hooks/useToast'
 
 import RouteModal from './RouteModal/RouteModal'
 
@@ -18,8 +20,11 @@ const PlaceRoute: FC<PlaceRouteProps> = ({ from, to }) => {
   const map = useMap()
   const routeRef = useRef<L.Polyline>(null)
   const dispatch = useAppDispatch()
+  const { addToast } = useToast()
   const [duration, setDuration] = useState<number>(0)
   const [distance, setDistance] = useState<number>(0)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [error, setError] = useState<boolean>(false)
 
   const handleCloseModal = () => {
     map.removeLayer(routeRef.current)
@@ -27,32 +32,42 @@ const PlaceRoute: FC<PlaceRouteProps> = ({ from, to }) => {
   }
 
   useEffect(() => {
-    getOSRMRoute(from, to).then(({ route, duration, distance }) => {
-      if (routeRef.current) {
-        map.removeLayer(routeRef.current)
-        routeRef.current = null
-      }
-      const latLngArr = route.map((coords) => {
-        if (Array.isArray(coords)) {
-          const [lng, lat] = coords
-          return [lat, lng] as LatLngExpression
-        } else if ('lat' in coords && 'lng' in coords) {
-          return [coords.lat, coords.lng] as LatLngExpression
+    setIsLoading(true)
+    setError(false)
+    getOSRMRoute(from, to)
+      .then(({ route, duration, distance }) => {
+        if (routeRef.current) {
+          map.removeLayer(routeRef.current)
+          routeRef.current = null
         }
+        const latLngArr = route.map((coords) => {
+          if (Array.isArray(coords)) {
+            const [lng, lat] = coords
+            return [lat, lng] as LatLngExpression
+          } else if ('lat' in coords && 'lng' in coords) {
+            return [coords.lat, coords.lng] as LatLngExpression
+          }
+        })
+
+        const routeLine = L.polyline(latLngArr, ROUTELINE_STYLE)
+
+        routeRef.current = routeLine
+        setDuration(duration)
+        setDistance(distance)
+        routeLine.addTo(map)
       })
+      .catch(() => {
+        addToast('Что-то пошло не так', 'error')
+        setError(true)
+      })
+      .finally(() => setIsLoading(false))
+  }, [addToast, from, map, to])
 
-      const routeLine = L.polyline(latLngArr, ROUTELINE_STYLE)
+  if (isLoading) {
+    return <Loader size="l" color="purple" />
+  }
 
-      routeRef.current = routeLine
-      setDuration(duration)
-      setDistance(distance)
-
-      routeLine.addTo(map)
-      map.fitBounds(routeLine.getBounds(), { padding: [50, 50] })
-    })
-  }, [from, map, to])
-
-  return (
+  return error ? null : (
     <RouteModal
       duration={duration}
       distance={distance}
